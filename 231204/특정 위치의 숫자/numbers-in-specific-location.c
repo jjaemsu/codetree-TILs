ca360ea58b931d4096bdb4d36c0b0bf649282963
jjaemsu <152167506+jjaemsu@users.noreply.github.com>
2023-12-04 00:48:20 +0900

    Title: 특정 위치의 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231204/특정 위치의 숫자/numbers-in-specific-location.c
+++ 231204/특정 위치의 숫자/numbers-in-specific-location.c
@@ -4,11 +4,13 @@ int main() {
     int x[10] = {0,};
     int hap = 0;
     for(int i = 0; i < 10; i++){
-        scanf("%d", x[i]);
-        hap += x[i];
+        scanf("%d", &x[i]);
     }
-
+    hap += x[2];
+    hap += x[4];
+    hap += x[9];
     printf("%d", hap);
+
 
 
     return 0;
