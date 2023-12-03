912e0cf253dc5221cb084afdbef787e57f3e0e3f
jjaemsu <152167506+jjaemsu@users.noreply.github.com>
2023-12-04 01:28:33 +0900

    Title: 대문자로 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231204/대문자로 바꾸기/change-to-capital.c
+++ 231204/대문자로 바꾸기/change-to-capital.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    char alphabet[5][3] = {
-        {'a','b','c'},
-        {'d','e','f'},
-        {'g','h','i'},
-        {'j','k','l'},
-        {'m','n','o'}
-    };
+    char alphabet[5][3] = {0,};
 
     for(int i = 0; i < 5; i++){
         for(int j = 0; j < 3; j++){ 
+            scanf("%c ", &alphabet[i][j]);
             alphabet[i][j] -= 32;
         }
     }
