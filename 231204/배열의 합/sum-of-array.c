a4f58b6f17cbcb413c1ff749236652ae3e6fadd1
jjaemsu <152167506+jjaemsu@users.noreply.github.com>
2023-12-04 01:17:13 +0900

    Title: 배열의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231204/배열의 합/sum-of-array.c
+++ 231204/배열의 합/sum-of-array.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int x[4][4] = {
-        {1, 2, 3, 4},
-        {7, 8, 9, 10},
-        {11, 12, 13, 14},
-        {15,16,17,18}
-    };
-
-    for (int i = 0; i < 4; i++) {
+    int x[4][4] = {0,};
+    for(int i = 0; i < 4; i++){
         int total = 0;
-        for (int j = 0; j < 4; j++) {  
+        for(int j = 0; j < 4; j++){
+            scanf("%d", &x[i][j]);
             total += x[i][j];
         }
         printf("%d\n", total);
     }
+
 
     return 0;
 }
