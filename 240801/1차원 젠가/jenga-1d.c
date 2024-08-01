717a0e640591df13e82c33acada42913b8248a56
jjaemsu <152167506+jjaemsu@users.noreply.github.com>
2024-08-01 21:49:05 +0900

    Title: 1차원 젠가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240801/1차원 젠가/jenga-1d.c
+++ 240801/1차원 젠가/jenga-1d.c
@@ -7,7 +7,6 @@ int main() {
     int n;
     int s1, e1, s2, e2;
 
-
     scanf("%d", &n);
 
     for (int i = 0; i < n; i++) {
@@ -15,47 +14,38 @@ int main() {
     }
 
     scanf("%d %d", &s1, &e1);
-    getchar();
     scanf("%d %d", &s2, &e2);
+
     int count = 0;
-    for (int i = s1 - 1; i <= e1 - 1; i++) {
-        arr[i] = 0;
-    }
-    for (int i = 0; i < n; i++) {
-        if (count > n) break;
 
-        while (arr[count] == 0) {
-            arr[count] = arr[count + 1];
-            count++;
+    for (int i = 0; i < n; i++) {
+        if (i < s1 - 1 || i > e1 - 1) {
+            tmp[count++] = arr[i];
         }
-        tmp[i] = arr[count];
-        count++;
-        arr[i] = tmp[i]; // 복사하기
     }
+
+    for (int i = 0; i < count; i++) {
+        arr[i] = tmp[i];
+    }
+
+    int new_n = count;  
 
     count = 0;
-    for (int i = s2 - 1; i <= e2 - 1; i++) {
-        arr[i] = 0;
-    }
-    for (int i = 0; i < n; i++) {
-        if (count > n) break;
-
-        while (arr[count] == 0) {
-            arr[count] = arr[count + 1];
-            count++;
+    for (int i = 0; i < new_n; i++) {
+        if (i < s2 - 1 || i > e2 - 1) {
+            tmp[count++] = arr[i];
         }
-        tmp[i] = arr[count];
-        count++;
-        arr[i] = tmp[i]; // 복사하기
     }
-    
-    count = 0;
 
-    printf("%d\n", n - (e1 - s1 + 1) - (e2 - s2 + 1));
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < count; i++) {
+        arr[i] = tmp[i];
+    }
+
+    printf("%d\n", count);
+    for (int i = 0; i < count; i++) {
         printf("%d\n", arr[i]);
     }
-
+    printf("\n");
 
     return 0;
 }
