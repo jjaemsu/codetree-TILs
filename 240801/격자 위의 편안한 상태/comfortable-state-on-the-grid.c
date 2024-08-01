6501a354464126a0f85b27f9154e683fcae160d5
jjaemsu <152167506+jjaemsu@users.noreply.github.com>
2024-08-01 18:15:53 +0900

    Title: 격자 위의 편안한 상태, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240801/격자 위의 편안한 상태/comfortable-state-on-the-grid.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+typedef enum{false, true} bool;
+
+bool in_range(int x, int y, int n) {
+    return 0 <= x && x < n && 0 <= y && y < n;
+}
+
+int main() {
+    int arr[100][100] = { 0, };
+    int n, m;
+    int dx[4] = { 0, 1, 0, -1 };
+    int dy[4] = { 1, 0, -1, 0 };
+
+    scanf("%d %d", &n, &m);
+
+    for (int i = 0; i < m; i++) {
+        int tmp = 0;
+        int r, c;
+        scanf("%d %d", &r, &c);
+        arr[r][c] = 1;
+        for (int j = 0; j < 4; j++) {
+            int nx = r + dx[j];
+            int ny = c + dy[j];
+            if (in_range(r, c, n) && arr[nx][ny] == 1) {
+                tmp++;
+            }
+        }
+        if (tmp == 3) {
+            printf("1\n");
+        }
+        else {
+            printf("0\n");
+        }
+    }
+
+    return 0;
+}
